d1361e1aad824a02348faf9feaeaf3ecb058dbc8
gashmit <ashmit.93025@gmail.com>
2025-03-24 00:12:06 +0530

    Final

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensures Tailwind scans your files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
